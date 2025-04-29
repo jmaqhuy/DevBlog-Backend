ff5a0c308abab4b86263512833c61c06bd3a5bb8
Quoc Huy <quochuy.ws@gmail.com>
2025-04-29 21:08:18 +0700

    format LocalDateTime

--- src/main/java/com/example/devblogbackend/dto/Meta.java
+++ src/main/java/com/example/devblogbackend/dto/Meta.java
@@ -1,6 +1,5 @@
 package com.example.devblogbackend.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -8,7 +7,6 @@ import java.time.LocalDateTime;
 @Data
 public class Meta {
     private String apiVersion;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "UTC")
     private LocalDateTime timestamp;
 
     public Meta(String apiVersion){
